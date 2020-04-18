dc682075e2b374af288ed7589f9afd7c9e3d38e0
AnyaSen <anyasenchikhina18@gmail.com>
2020-04-18 15:56:34 +0300

    Added loading and error pages and contexts

---
+++ src/pages/ErrorCard/ErrorCard.js
@@ -0,0 +1,21 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+import Styles from "./ErrorCard.module.scss";
+
+import errorIcon from "../../assets/img/error.svg";
+import SecondaryButton from "../../components/Buttons/SecondaryButton/SecondaryButton";
+
+export default function ErrorCard() {
+  return (
+    <div className={Styles.ErrorCardContainer}>
+      <img src={errorIcon} alt="Error" />
+
+      <h3>Sorry, something went wrong</h3>
+
+      <Link to="/">
+        <SecondaryButton btnText="Go back" />
+      </Link>
+    </div>
+  );
+}
---
+++ src/pages/LoadingPage/LoadingPage.js
@@ -0,0 +1,13 @@
+import React from "react";
+import Styles from "./LoadingPage.module.scss";
+
+import loader from "../../assets/img/loader.svg";
+
+export default function LoadingPage() {
+  return (
+    <div data-testid="Loader" className={Styles.LoadingPage}>
+      <img src={loader} alt="loader icon" />
+      <h3>Loading...</h3>
+    </div>
+  );
+}
